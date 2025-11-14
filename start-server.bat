dfbcbbbf954251e393b4573b8f95071cb7f42fc2
miao0221 <1046781739@qq.com>
2025-11-14 21:33:49 +0800

    2133

---
+++ start-server.bat
@@ -0,0 +1,5 @@
+@echo off
+echo 启动本地开发服务器...
+echo 请在浏览器中访问 http://localhost:8000 查看网站
+python -m http.server 8000
+pause
---
+++ start-server.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "启动本地开发服务器..."
+echo "请在浏览器中访问 http://localhost:8000 查看网站"
+python3 -m http.server 8000
